04cbe716c286394fc917882e8256c7c83dd79484
Douglas16Vieira <Douglas16Vieira@users.noreply.github.com>
2026-09-14 21:30:45 -0300

    cria infraestrutura do agendador
    Criação da infraestrutura para criação de site agendador de consultas.Implementa função buscarDados com fetch para /available_slotsConfigura integração com main.py

---
+++ static/script.js
@@ -0,0 +1,28 @@
+const Busca = document.getElementById('btn-buscar');
+const AgendarBtn = document.getElementById('agendar');
+const telCont = document.getElementById('telefone');
+const nomeUs = document.getElementById('nome');
+const HDisponivel = document.getElementById('horarios');
+const dataAgen = document.getElementById('data');
+
+Busca.addEventListener('click', buscarDados);
+AgendarBtn.addEventListener('click', AgendarData);
+
+function buscarDados() {
+    const data = dataAgen.value;
+    const url =  '/available_slots?date=${data}';
+
+        fetch(url)
+        .then(res => res.json())
+        .then(horarios => {
+           HDisponivel.innerHTML = '';
+
+           horarios.forEach(hora => {
+                const option = document.createElement('option');
+                option.value = hora;
+                option.textContent = hora;
+                HDisponivel.appendChild(option);
+            });
+        });
+
+}
